61cdc4a7ed3ca7155f9172ab750225dcd834e82d
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-10 09:14:58 +0300

    initial commit

---
+++ src/main/java/com/salatin/similarsearch/SimilarSearchApplication.java
@@ -0,0 +1,13 @@
+package com.salatin.similarsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salatin/similarsearch/SimilarSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salatin.similarsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
