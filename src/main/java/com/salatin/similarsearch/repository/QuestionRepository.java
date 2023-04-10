ab59ff2a232ce1b453289a6e795de1ac85fbf3e9
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-10 10:06:33 +0300

    QuestionRepository was created

---
+++ src/main/java/com/salatin/similarsearch/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.salatin.similarsearch.repository;
+
+import com.salatin.similarsearch.model.Question;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query("SELECT q FROM Question q ORDER BY LENGTH(q.text) DESC")
+    List<Question> findTopXByOrderByLengthDesc(int limit);
+}
