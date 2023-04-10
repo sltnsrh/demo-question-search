8a44961a657f6aa32ca1cc9b3816ddd6b69e1355
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-10 12:05:53 +0300

    path param count validation was fixed

--- src/main/java/com/salatin/similarsearch/controller/QuestionController.java
+++ src/main/java/com/salatin/similarsearch/controller/QuestionController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/questions")
 @RequiredArgsConstructor
+@Validated
 public class QuestionController {
     private final QuestionService questionService;
 
---
+++ src/main/java/com/salatin/similarsearch/exception/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.salatin.similarsearch.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    private final static String DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<ApiExceptionObject> handleBadRequestException(
+        RuntimeException e
+    ) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+    }
+
+    private ApiExceptionObject getApiExceptionObject(String message, HttpStatus status) {
+        return new ApiExceptionObject(
+            message,
+            status,
+            LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN))
+        );
+    }
+}
