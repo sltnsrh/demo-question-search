8dde91d789dbdbf59f6d24cc313a6bcd1ce759a8
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-10 10:38:00 +0300

    Question controller was created with method findTopLongest

---
+++ src/main/java/com/salatin/similarsearch/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.salatin.similarsearch.controller;
+
+import com.salatin.similarsearch.model.Question;
+import com.salatin.similarsearch.service.QuestionService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/questions")
+@RequiredArgsConstructor
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping
+    public ResponseEntity<List<Question>> findTopLongest(@RequestParam int count) {
+        return new ResponseEntity<>(questionService.findTopLongestQuestions(count), HttpStatus.OK);
+    }
+}
