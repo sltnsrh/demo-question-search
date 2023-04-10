19fd0826ef630cde7b4db0c51d1d68f7a360b2df
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-10 18:04:51 +0300

    QuestionServiceImpl was refactored

--- src/main/java/com/salatin/similarsearch/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/salatin/similarsearch/service/impl/QuestionServiceImpl.java
@@ -9,15 +9,24 @@ import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class QuestionServiceImpl implements QuestionService {
     private final static String NOT_SYMBOLS_PATTERN = "[^a-zA-Z\\s]";
     private final static String WHITESPACES_PATTERN = "\\s+";
+    private final static String WHITESPACE_SYMBOL = " ";
+    private final static short WORD_SIZE_MIN = 3;
 
     private final QuestionRepository questionRepository;
 
@@ -28,62 +37,108 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public List<Question> findTopSimilar(int count, String question) {
-        String firstWord = (
-            question.replaceAll(NOT_SYMBOLS_PATTERN, WHITESPACES_PATTERN)
-            .trim()
-            .split(WHITESPACES_PATTERN)[0] + " "
-        )
-            .toLowerCase();
+        long timeStarted = System.currentTimeMillis();
+        log.info(String.format("Searching for a question: '%s'", question));
 
+        String firstWord = getFirstWord(question);
         List<Question> candidates = questionRepository.findByWordStartingWith(firstWord);
-
         Queue<SimilarQuestion> similarQuestions = new PriorityQueue<>(Collections.reverseOrder());
+        ExecutorService executorService = null;
 
-        candidates.forEach(candidate -> {
-                double similarity = calculateSimilarity(question, candidate.getText());
-                similarQuestions.add(new SimilarQuestion(similarity, candidate));
+        try {
+            executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            CompletionService<SimilarQuestion> completionService = new ExecutorCompletionService<>(executorService);
+
+            candidates.forEach(candidate -> completionService.submit(
+                () -> {
+                    double similarity = calculateSimilarity(question, candidate.getText());
+                    return new SimilarQuestion(similarity, candidate);
+                })
+            );
+
+            gatherSimilarQuestions(similarQuestions, completionService, candidates.size());
+
+        } finally {
+            if (executorService != null) {
+                executorService.shutdown();
             }
-        );
-
-        if (!similarQuestions.isEmpty() && similarQuestions.peek().getSimilarity() > 0) {
-            return similarQuestions.stream()
-                .filter(s -> s.getSimilarity() > 0)
-                .map(SimilarQuestion::getQuestion)
-                .limit(count)
-                .toList();
         }
 
-        Question newQuestion = new Question();
-        newQuestion.setText(question);
+        log.info(String.format("Finished searching for a question: '%s' in %s ms",
+            question,
+            System.currentTimeMillis() - timeStarted));
 
-        questionRepository.save(newQuestion);
+        return getSimilarQuestionsLimitedList(similarQuestions, question, count);
+    }
 
-        return List.of();
+    private String getFirstWord(String question) {
+        return (question.replaceAll(NOT_SYMBOLS_PATTERN, WHITESPACE_SYMBOL)
+                .trim()
+                .split(WHITESPACES_PATTERN)[0] + WHITESPACE_SYMBOL
+        )
+            .toLowerCase();
     }
 
     private double calculateSimilarity(String queryQuestion, String candidateQuestion) {
         String[] queryWords = queryQuestion
             .trim()
-            .replaceAll(NOT_SYMBOLS_PATTERN, " ")
+            .replaceAll(NOT_SYMBOLS_PATTERN, WHITESPACE_SYMBOL)
             .split(WHITESPACES_PATTERN);
 
         String[] candidateWords = candidateQuestion
             .trim()
-            .replaceAll(NOT_SYMBOLS_PATTERN, " ")
+            .replaceAll(NOT_SYMBOLS_PATTERN, WHITESPACE_SYMBOL)
             .split(WHITESPACES_PATTERN);
 
         AtomicInteger totalWords = new AtomicInteger();
 
         int commonWords = (int) Arrays.stream(queryWords)
-            .filter(word1 -> word1.length() > 3)
+            .filter(word1 -> word1.length() > WORD_SIZE_MIN)
             .peek(word -> totalWords.incrementAndGet())
             .flatMap(word1 -> Arrays.stream(candidateWords)
-                .filter(word2 -> word2.length() > 3)
+                .filter(word2 -> word2.length() > WORD_SIZE_MIN)
                 .filter(word1::equalsIgnoreCase)
             )
             .distinct()
             .count();
 
         return (double) commonWords / totalWords.get();
+    }
+
+    private void gatherSimilarQuestions(Queue<SimilarQuestion> similarQuestions,
+                                        CompletionService<SimilarQuestion> completionService,
+                                        int numCandidates) {
+        for (int i = 0; i < numCandidates; i++) {
+            try {
+                SimilarQuestion similarQuestion = completionService.take().get();
+                if (similarQuestion.getSimilarity() > 0) {
+                    similarQuestions.add(similarQuestion);
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private List<Question> getSimilarQuestionsLimitedList(Queue<SimilarQuestion> similarQuestions,
+                                                          String question,
+                                                          int count) {
+        if (similarQuestions.isEmpty()) {
+            createAndSaveNewQuestion(question);
+
+            return List.of();
+        }
+
+        return similarQuestions.stream()
+            .map(SimilarQuestion::getQuestion)
+            .limit(count)
+            .toList();
+    }
+
+    private void createAndSaveNewQuestion(String question) {
+        Question newQuestion = new Question();
+        newQuestion.setText(question);
+
+        questionRepository.save(newQuestion);
     }
 }
